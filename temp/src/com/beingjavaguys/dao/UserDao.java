affa37b4e17e9be29fa8dd6d374e42115985b93a
Feng <fenglhs@gmail.com>
2014-09-23 15:25:07 -0400

    Feng's project with Spring on 722
    rt

---
+++ temp/src/com/beingjavaguys/dao/UserDao.java
@@ -0,0 +1,10 @@
+
+package com.beingjavaguys.dao;
+
+import java.util.List;
+import com.beingjavaguys.domain.User;
+
+public interface UserDao {
+public void saveUser ( User user );
+public List<User> getUser();
+}
